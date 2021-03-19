217adb719abb283090f7c590f0a7366896f01129
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-03-19 16:39:47 +0100

    updated read

--- api/posts/read.php
+++ api/posts/read.php
@@ -20,15 +20,37 @@ $result = $post->read();
 
 $num = $result->num_rows();
 
+// get row count
 if($num > 0){
 //post array
 
-$postArr = array();
-$postArr['data'] = array();
+$post_arr = array();
+$post_arr['data'] = array();
 
-while($row = $result->fetch()){
+while($row = $result->fetch_array()){
+    estract($row);
 
+    $post_item = array(
+        'id' => $id,
+        'title' => $title,
+        'body' => html_entity_decode($body),
+        'author' => $author,
+        'cartegory_id' => $category_id,
+        'category_name' => $category_name
+    );
+
+    // push to data
+    array_push($post_aarr['data'], $post_item);
 }
+
+// turn into json & output
+    echo json_encode($post_arr);
+}
+else{
+    // no posts
+    echo json_encode(
+        array('message' => 'No post found')
+    );
 }
 
 ?>
